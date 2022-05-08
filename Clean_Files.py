131f33860c7ea40ba7f1eec6bc361b2a7713c379
Nelwade <nelsonowade@gmail.com>
2022-05-08 23:41:26 +0300

    This commit improves efficiency and adds window notifications

--- Clean_Files.py
+++ Clean_Files.py
@@ -4,12 +4,16 @@ Created on Fri Apr  8 21:04:14 2022
 
 @author: Owade
 """
+from win10toast import ToastNotifier
+
 import os
 import sys
 import clean_tvshows
 import clean_movies
 import clean_music
 import datetime
+
+notification = ToastNotifier()
 
 # main_dir name of the directory where you download your tv shows into, per episode
 # it can take a user input
@@ -20,22 +24,25 @@ def clean_disk(main_dir):
     time = datetime.datetime.now()
     print("\n\n", time, "\n-----------------------------------\n")
 
-    clean_tvshows.clean_tvshows(main_dir) 
+    clean_tvshows.clean_tvshows(main_dir)
+    clean_music.clean_music(main_dir) 
     clean_movies.clean_movies(main_dir) 
-    clean_music.clean_music(main_dir)
+    
     
 # clean_disk("D:/")
 
 
 # Manually creating logs to track all actions the program takes
 # The path will vary according to where you want to store your logs
-if os.path.exists(r'C:\Users\Stewie\Dropbox\My PC (DESKTOP-57VNQ1O)\Documents\Python projects\Clean Files\logs.txt'):
+if os.path.exists(r'D:Python projects\Clean Files\logs.txt'):
         with open('logs.txt', 'a', encoding="utf-8") as logs:
             sys.stdout = logs
-            clean_disk("D:/")
+            clean_disk(r"D:/")
     
 else:
-    os.chdir(r'C:\Users\Stewie\Dropbox\My PC (DESKTOP-57VNQ1O)\Documents\Python projects\Clean Files')
+    os.chdir(r'D:\Python projects\Clean Files')
     with open('logs.txt', 'w', encoding="utf-8") as logs:
         sys.stdout = logs
-        clean_disk("D:/")
+        clean_disk(r"D:/")
+
+notification.show_toast("Clean Files", "Terminating Clean Files", duration = 30)
--- clean_tvshows.py
+++ clean_tvshows.py
@@ -1,12 +1,24 @@
+from glob import glob
+from win10toast import ToastNotifier
+
 import os
 import string
 import shutil
 import sys
 import is_download_complete
 
+notification = ToastNotifier() # creates a window notification
+
+def notification_error():
+    try:
+        notification.show_toast("Clean Files", str(sys.exc_info), duration = 60)
+    except:
+        print(str(sys.exc_info()))
+
 def clean_tvshows(main_dir):
     
     print("Cleaning Tv Show Files........")
+    notification.show_toast("Clean Files", "Cleaning Tv Files", duration = 30)
 
     # change into the directory to allow creation of directories and moving of files
     os.chdir(main_dir)
@@ -14,70 +26,68 @@ def clean_tvshows(main_dir):
     #check if the folder named Series exists, if it does not create the folder
     if os.path.exists('Series'):
         current_path = os.path.join(main_dir, 'Series')
-    else: 
+    else:
         os.mkdir('Series')
         current_path = os.path.join(main_dir, 'Series')
-        print("Creating Series Folder")
+        print("\n Did not find Series Folder, Creating Series Folder")
     
-    
+    downloaded_shows = glob('*S0*') + glob('*season*') # filters just tv show and returns a list
     # Iterate through the contents of the directory
-    for item in os.listdir(main_dir):
+    for item in downloaded_shows:
         try:
+            if is_download_complete.is_download_complete(main_dir, item):              
 
-            if item.endswith('mkv') or item.endswith('mp4') or 'S0' in item or 'Season' and 'Complete' in item:
-                # Filter out videos and Tv shows in folders with "
-                if is_download_complete.is_download_complete(main_dir, item):              
+                folder_name = ""
 
-                    folder_name = ""
-
-                    if "." in item:
-                        name = item.split(".")
-                    else:
-                        name = item.split()
-                        name.remove("Season")
-
-                    for word in name:
-                        word_punc = word.translate(str.maketrans(
-                            '', '', string.punctuation))  # remove punctuation marks
-                        if word_punc.isalpha():
-                            folder_name += word + " "  # parse to leave a short name of tv show
-                        else:
-                            break
-
-                    os.chdir(current_path) #change into the series folder
-
-                    if os.path.exists(folder_name):
-                        
-                        # if a folder for the tv show already exists, copy into folder
-                        sourcepath = os.path.join(main_dir, item.strip())
-                        destpath = os.path.join(current_path, folder_name.strip())
-                        
-                        os.chdir(destpath) #change into tv show folder
-
-                        if os.path.exists(item): # check if movie already exists in the movies folder
-                                print("{} already exists in folder, Deleting".format(item))
-                                
-                                if os.path.isdir(item):
-                                    shutil.rmtree(os.path.join(sourcepath)) # delete if directory
-                                else: 
-                                    os.remove(sourcepath) #delete if file
-                        else:
-                            shutil.move(sourcepath, destpath)
-                            print("\nFolder exists, copying {} to {} folder".
-                                    format(item, folder_name))
-                    else:
-                        print('\nCreating Folder named {}...'.format(folder_name))
-                        # if folder does not exist, create folder
-                        os.mkdir(folder_name)
-                        # and copy into folder
-                        print('\nNow copying {} to {} folder\n'.format(
-                            item, folder_name))
-
-                        sourcepath = os.path.join(main_dir, item.strip())
-                        destpath = os.path.join(current_path, folder_name.strip())
-                        shutil.move(sourcepath, destpath)
+                if "." in item:
+                    name = item.split(".")
                 else:
-                    print('\nDownload of show {} is not complete\n'.format(item))
+                    name = item.split()
+                    name.remove("Season")
+
+                for word in name:
+                    word_punc = word.translate(str.maketrans(
+                        '', '', string.punctuation))  # remove punctuation marks
+                    if word_punc.isalpha():
+                        folder_name += word + " "  # parse to leave a short name of tv show
+                    else:
+                        break
+
+                os.chdir(current_path) #change into the series folder
+
+                if os.path.exists(folder_name):
+                    
+                    # if a folder for the tv show already exists, copy into folder
+                    sourcepath = os.path.join(main_dir, item.strip())
+                    destpath = os.path.join(current_path, folder_name.strip())
+                    
+                    os.chdir(destpath) #change into tv show folder
+
+                    if os.path.exists(item): # check if movie already exists in the movies folder
+                            print("{} already exists in folder, Deleting".format(item))
+                            
+                            if os.path.isdir(item):
+                                shutil.rmtree(os.path.join(sourcepath)) # delete if directory
+                            else: 
+                                os.remove(sourcepath) #delete if file
+                    else:
+                        shutil.move(sourcepath, destpath)
+                        print("\nFolder exists, copying {} to {} folder".
+                                format(item, folder_name))
+                else:
+                    print('\nCreating Folder named {}...'.format(folder_name))
+                    # if folder does not exist, create folder
+                    os.mkdir(folder_name)
+                    # and copy into folder
+                    print('\nNow copying {} to {} folder\n'.format(
+                        item, folder_name))
+
+                    sourcepath = os.path.join(main_dir, item.strip())
+                    destpath = os.path.join(current_path, folder_name.strip())
+                    shutil.move(sourcepath, destpath)
+            else:
+                print('\nDownload of show {} is not complete\n'.format(item))
         except:
             print("{} did not move because of {}".format(
                 item, sys.exc_info()))
+            notification_error()
