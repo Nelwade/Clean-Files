0e06a45ff0a9effe3ddf8e2d1b1a0b9d788ad98e
Owade <82964062+Nelwade@users.noreply.github.com>
2022-08-11 23:46:33 +0300

    Update is_download_complete.py

--- is_download_complete.py
+++ is_download_complete.py
@@ -4,18 +4,25 @@ import os
 def is_download_complete(main_dir, item):  # Function to check if download is complete
     
     current_path = os.path.join(main_dir, item)
-    if item.lower().endswith('!ut'):
-        return False
+    if os.path.isfile(current_path):
+        if "!ut" in item.lower():
+            #print(item)
+            return False
+        
 
     elif os.path.isdir(current_path):  # check if folders contain files still being torrented
         # os.chdir(item)
         for file in os.listdir(current_path):
-            #print(file)
-            next_path = os.path.join(current_path, file)
-            return is_download_complete(next_path, file) # employ recursivity for folders within folders
-            # if file.lower().endswith('!ut'):
-            #     return False
-        return True
-    else:
-        return True
+            #next_path = os.path.join(current_path, file)
+            if is_download_complete(current_path, file) == False:
+                return False
+            # return is_download_complete(current_path, file)
+    return True
 
+
+# for item in os.listdir("E:/"):
+#     try:
+#         result = is_download_complete("E:/", item)
+#         print("{}:   {}".format(item, result))
+#     except PermissionError as error:
+#         print("{}:   {}".format(item, error))
